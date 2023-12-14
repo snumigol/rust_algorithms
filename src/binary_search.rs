081d8fe1d812b81ff7a510c57866a8a8eed01340
snumigol <serhiiyahotin@gmail.com>
2023-12-14 16:37:18 +0000

    binary search

--- src/binary_search.rs
+++ src/binary_search.rs
@@ -2,14 +2,13 @@ pub fn binary_search(haystack: Vec<i32>, needle: i32) -> bool {
     
     let mut low = 0;
     let mut high = haystack.len() - 1;
-    let target = needle;
 
     while low <= high {
         let mid = low + (high - low) / 2;
 
-        if haystack[mid] == target {
+        if haystack[mid] == needle {
             return true
-        } else if haystack[mid] < target {
+        } else if haystack[mid] < needle {
             low = mid + 1;
         } else {
             high = mid - 1;
